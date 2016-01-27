6ff5bec298e90ac951069334de56541690448286
van1988ch1 <van1988ch1@gmail.com>
2016-01-27 18:19:10 +0800

    微博

---
+++ 黑马/AppDelegate.m
@@ -0,0 +1,80 @@
+//
+//  AppDelegate.m
+//  黑马
+//
+//  Created by van1988ch on 16/1/27.
+//  Copyright © 2016年 van1988ch. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+#define RandomColor [UIColor colorWithRed:arc4random_uniform(256)/255.0 green:arc4random_uniform(256)/255.0 blue:arc4random_uniform(256)/255.0  alpha:1.0];
+#define HWColor(r , g , b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
+
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] init];
+    self.window.frame = [UIScreen mainScreen].bounds;
+    
+    UITabBarController * tabbarVc = [[UITabBarController alloc] init];
+    self.window.rootViewController = tabbarVc;
+    
+    UIViewController *vc1 = [self addChildVC:@"首页" image:@"tabbar_home" selectedImage:@"tabbar_home_selected"];
+    UIViewController *vc2 = [self addChildVC:@"消息" image:@"tabbar_message_center" selectedImage:@"tabbar_message_center_selected"];
+    UIViewController *vc3 = [self addChildVC:@"发现" image:@"tabbar_discover" selectedImage:@"tabbar_discover_selected"];
+    UIViewController *vc4 = [self addChildVC:@"我" image:@"tabbar_profile" selectedImage:@"tabbar_profile_selected"];
+
+    tabbarVc.viewControllers = @[vc1 , vc2 , vc3 , vc4];
+    
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+- (UIViewController *) addChildVC:(NSString *)title image:(NSString *)image selectedImage:(NSString*)selectedImage{
+    UIViewController *childVc = [[UIViewController alloc] init];
+    childVc.tabBarItem.title = title;
+    childVc.tabBarItem.image = [UIImage imageNamed:image];
+    childVc.tabBarItem.selectedImage  = [[UIImage imageNamed:selectedImage]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    NSMutableDictionary *textattrs = [NSMutableDictionary dictionary];
+    textattrs[NSForegroundColorAttributeName] = HWColor(123, 123, 123);
+    
+    NSMutableDictionary *selecttextattrs = [NSMutableDictionary dictionary];
+    selecttextattrs[NSForegroundColorAttributeName] = [UIColor orangeColor];
+    
+    [childVc.tabBarItem setTitleTextAttributes:textattrs forState:UIControlStateNormal];
+    [childVc.tabBarItem setTitleTextAttributes:selecttextattrs forState:UIControlStateSelected];
+    childVc.view.backgroundColor = RandomColor;
+    return childVc;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
---
+++ 黑马/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  黑马
+//
+//  Created by van1988ch on 16/1/27.
+//  Copyright © 2016年 van1988ch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
