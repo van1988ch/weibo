ea4edd6a75f86308e79142420118617645044dd8
van1988ch1 <van1988ch1@gmail.com>
2016-01-28 15:14:32 +0800

    UIView扩展

---
+++ 黑马/UIView+UIViewExtentsion.h
@@ -0,0 +1,13 @@
+//
+//  UIView+UIViewExtentsion.h
+//  黑马
+//
+//  Created by van1988ch on 16/1/28.
+//  Copyright © 2016年 van1988ch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (UIViewExtentsion)
+
+@end
---
+++ 黑马/UIView+UIViewExtentsion.m
@@ -0,0 +1,13 @@
+//
+//  UIView+UIViewExtentsion.m
+//  黑马
+//
+//  Created by van1988ch on 16/1/28.
+//  Copyright © 2016年 van1988ch. All rights reserved.
+//
+
+#import "UIView+UIViewExtentsion.h"
+
+@implementation UIView (UIViewExtentsion)
+
+@end
